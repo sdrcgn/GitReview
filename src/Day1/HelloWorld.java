d7e50d303db48fce974f45cb679c5ac8c7d5e042
Sidar Cetinkaya <sidar457@icloud.com>
2022-09-02 08:35:05 -0400

    by java

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -3,5 +3,6 @@ package Day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Maybe 5 weeks of Java left");
     }
 }
